d2f0892a88e555869c92a82230b48a100b0343e2
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-03-09 01:25:30 +0800

    remainder when the first number is divided by the second number

---
+++ remainder_division.py
@@ -0,0 +1,9 @@
+# Ask the user for two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Get the remainder when first number is divided by second number
+remainder = num1 % num2
+
+# Display the remainder
+print("The remainder is:", remainder)
