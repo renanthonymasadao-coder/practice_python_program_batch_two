1e08b957e95f547f50a65ce43d9cd92c7b384e93
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-03-09 00:10:53 +0800

    smaller numbers if num1 or num2

---
+++ smaller_number.py
@@ -0,0 +1,9 @@
+#Ask the user for two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+#Compare the numbers if smaller
+if num1 < num2:
+    print("The num1 are smaller than num2")
+elif num2 < num1:
+    print("The num2 are smaller than num1")
