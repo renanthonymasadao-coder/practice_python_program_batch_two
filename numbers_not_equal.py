54750810a222895233b97a141c032ccb4c74424c
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-03-09 00:42:29 +0800

    checking if the numbers are not equal it it not same number

---
+++ numbers_not_equal.py
@@ -0,0 +1,7 @@
+#Ask the user for two numbers
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+#Check if the numbers are not equal
+if number1 != number2:
+    print("The numbers are not equal")
