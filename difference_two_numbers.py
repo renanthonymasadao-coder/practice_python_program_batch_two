1f7e1293b712a66180fd14891f71e034694b28bc
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-03-09 01:01:06 +0800

    getting the difference of two numbers

---
+++ difference_two_numbers.py
@@ -0,0 +1,9 @@
+#Ask the user for two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+#Subtract the numbers to see the difference
+difference = num1 - num2
+
+#Display the difference
+print("The difference is:", difference)
