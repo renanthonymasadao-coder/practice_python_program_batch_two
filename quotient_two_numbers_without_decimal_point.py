488b1db7f67845e9fda577b130d7456925726c05
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-03-09 01:11:52 +0800

    dividing the two numbers withouy decimal point

---
+++ quotient_two_numbers_without_decimal_point.py
@@ -0,0 +1,9 @@
+#Ask the user for two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+#Divide the two numbers
+quotient =  num1 // num2
+
+#Display the quotient
+print("The quotient is:", quotient)
